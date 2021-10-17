b4a2af8f00d1883a39dbcb7d4659237aea9a19d5
GH0STAV0 <91119949+GH0STAV0@users.noreply.github.com>
2021-10-17 23:19:29 +0100

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -12,13 +12,13 @@ echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
 wget http://launchpadlibrarian.net/494460182/xfwm4_4.14.5-1_amd64.deb
 dpkg -i xfwm4_4.14.5-1_amd64.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1 python3-pip python3.9-tk python3.9-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing ffmpeg supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1 python3-pip python3.9-tk python3.9-dev
 
 echo "Install Package -firefox-53.0b9.tar.bz2"
-wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/firefox-53.0b9.tar.bz2 #&& tar -xf firefox-53.0b9.tar.bz2 -C /root/EXTRAT/firefox-53.0b9/
-wget https://ftp.mozilla.org/pub/firefox/releases/57.0.1/linux-x86_64/en-GB/firefox-57.0.1.tar.bz2 #&& tar -xf firefox-57.0.1.tar.bz2 -C /root/EXTRAT/firefox-57.0.1/
-wget https://ftp.mozilla.org/pub/firefox/releases/58.0.1/linux-x86_64/en-GB/firefox-58.0.1.tar.bz2 #&& tar -xf firefox-58.0.1.tar.bz2 -C /root/EXTRAT/firefox-58.0.1/
-wget https://ftp.mozilla.org/pub/firefox/releases/59.0.1/linux-x86_64/en-GB/firefox-59.0.1.tar.bz2 #&& tar -xf firefox-59.0.1.tar.bz2 -C /root/EXTRAT/firefox-59.0.1/
+wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/firefox-53.0b9.tar.bz2 && tar -xf firefox-53.0b9.tar.bz2 -C /root/EXTRAT/firefox-53.0b9/
+wget https://ftp.mozilla.org/pub/firefox/releases/57.0.1/linux-x86_64/en-GB/firefox-57.0.1.tar.bz2 && tar -xf firefox-57.0.1.tar.bz2 -C /root/EXTRAT/firefox-57.0.1/
+wget https://ftp.mozilla.org/pub/firefox/releases/58.0.1/linux-x86_64/en-GB/firefox-58.0.1.tar.bz2 && tar -xf firefox-58.0.1.tar.bz2 -C /root/EXTRAT/firefox-58.0.1/
+wget https://ftp.mozilla.org/pub/firefox/releases/59.0.1/linux-x86_64/en-GB/firefox-59.0.1.tar.bz2 && tar -xf firefox-59.0.1.tar.bz2 -C /root/EXTRAT/firefox-59.0.1/
 
 
 echo "Install bash color"

417bc27f42b07268877e3853174b793d26ce7b2c
GH0STAV0 <91119949+GH0STAV0@users.noreply.github.com>
2021-10-25 14:41:19 -0700

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -13,6 +13,7 @@ wget http://launchpadlibrarian.net/494460182/xfwm4_4.14.5-1_amd64.deb
 dpkg -i xfwm4_4.14.5-1_amd64.deb
 apt update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing ffmpeg supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1 python3-pip python3.9-tk python3.9-dev
+mkdir /root/EXTRAT/firefox-53.0.2/
 mkdir /root/EXTRAT/firefox-53.0b9/
 mkdir /root/EXTRAT/firefox-57.0.1/
 mkdir /root/EXTRAT/firefox-58.0.1/
@@ -20,8 +21,9 @@ mkdir /root/EXTRAT/firefox-59.0.1/
 mkdir /root/EXTRAT/firefox-60.0.1esr/
 mkdir /root/EXTRAT/firefox-61.0.1/
 mkdir /root/EXTRAT/firefox-63.0.1/
-
+#https://ftp.mozilla.org/pub/firefox/releases/53.0.2/linux-x86_64-EME-free/en-GB/firefox-53.0.2.tar.bz2
 echo "Install Package -firefox-53.0b9.tar.bz2"
+wget https://ftp.mozilla.org/pub/firefox/releases/53.0.2/linux-x86_64-EME-free/en-GB/firefox-53.0.2.tar.bz2 && tar -xf firefox-53.0.2.tar.bz2 -C /root/EXTRAT/firefox-53.0.2/
 wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/firefox-53.0b9.tar.bz2 && tar -xf firefox-53.0b9.tar.bz2 -C /root/EXTRAT/firefox-53.0b9/
 wget https://ftp.mozilla.org/pub/firefox/releases/57.0.1/linux-x86_64/en-GB/firefox-57.0.1.tar.bz2 && tar -xf firefox-57.0.1.tar.bz2 -C /root/EXTRAT/firefox-57.0.1/
 wget https://ftp.mozilla.org/pub/firefox/releases/58.0.1/linux-x86_64/en-GB/firefox-58.0.1.tar.bz2 && tar -xf firefox-58.0.1.tar.bz2 -C /root/EXTRAT/firefox-58.0.1/
@@ -29,6 +31,8 @@ wget https://ftp.mozilla.org/pub/firefox/releases/59.0.1/linux-x86_64/en-GB/fire
 wget https://ftp.mozilla.org/pub/firefox/releases/60.0.1esr/linux-x86_64/en-US/firefox-60.0.1esr.tar.bz2 && tar -xf firefox-60.0.1esr.tar.bz2 -C /root/EXTRAT/firefox-60.0.1esr/
 wget https://ftp.mozilla.org/pub/firefox/releases/61.0.1/linux-x86_64/en-US/firefox-61.0.1.tar.bz2 && tar -xf firefox-61.0.1.tar.bz2 -C /root/EXTRAT/firefox-61.0.1/
 wget https://ftp.mozilla.org/pub/firefox/releases/63.0.1/linux-x86_64/en-US/firefox-63.0.1.tar.bz2 && tar -xf firefox-63.0.1.tar.bz2 -C /root/EXTRAT/firefox-63.0.1/
+rm *.tar.bz2
+
 echo "Install bash color"
 echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /root/.bashrc
 chmod +x /root/install_add/geckodriver && cp /root/install_add/geckodriver /usr/bin/geckodriver13

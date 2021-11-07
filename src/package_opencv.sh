22e988b1324a5867c046bb5ae22440840ae7ebd3
GH0STAV0 <91119949+GH0STAV0@users.noreply.github.com>
2021-11-07 15:23:59 -0800

    Update package_opencv.sh

--- src/package_opencv.sh
+++ src/package_opencv.sh
@@ -5,7 +5,7 @@ set -e
 echo "Install Package - on  based ubuntu 20.04 LTS"
 DEBIAN_FRONTEND=noninteractive apt-get install -y libsm6 libxrender1 libfontconfig1
 pip3 install numpy --upgrade
-pip3 install image pandas Pillow==8.2.0 opencv-python easyocr #cv2
+pip3 install image pandas Pillow==8.2.0 opencv-python easyocr pytesseract #cv2
 
 
 
